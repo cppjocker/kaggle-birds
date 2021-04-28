03afe20e81db0a7220f225a6a80052b50ab67fb7
cppjocker <gihub@github>
2021-04-28 17:44:00 +0700

    preparing comb model

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPool1D, BatchNormal
 from keras.optimizers import RMSprop, Adam
 from keras.applications import VGG19, VGG16, ResNet50
 
+import tensorflow as tf
+from keras import backend as KK
 
 def next_power_of_2(x):
     return 1 if x == 0 else 2 ** (x - 1).bit_length()
@@ -61,9 +63,13 @@ class DataGenerator(Sequence):
 
         X = np.zeros((self.batch_size, half_spec_size, 160))
         y = np.zeros((self.batch_size, self.num_labels))
+
+        #remove this
+        y = np.zeros((self.batch_size, 1))
+
         last_file = ''
 
-        print(list_IDs_temp)
+        #print(list_IDs_temp)
 
         for i, ID in enumerate(list_IDs_temp):
             prefix = str(self.data.loc[ID, 'audio_id']) + '_' + self.data.loc[ID, 'site']
@@ -91,6 +97,7 @@ class DataGenerator(Sequence):
                 # audio_file_fft = (audio_file_fft - audio_file_fft.mean()) / audio_file_fft.std()
             X[i,] = audio_file_fft
             y[i,] = self.data.loc[ID, self.data.columns[5:]].values
+
         return X, y, spec_size
 
 
@@ -134,6 +141,7 @@ def main():
 
     train_idxs, val_idxs = train_test_split(list(train_labels.index), test_size=0.33, random_state = 100)
     test_idxs = list(samp_subm.index)
+    all_train_idxs = list( train_labels.index )
 
     num_labels = len(labels)
     batch_size = 30
@@ -145,37 +153,46 @@ def main():
                                   data_length=data_length, num_labels=num_labels)
     test_generator = DataGenerator(path + 'test_soundscapes/', test_idxs, test_data, batch_size=1,
                                    data_length=data_length, num_labels=num_labels)
-    if False:
-        model = Sequential()
-        model.add(Conv1D(64, input_shape=(train_generator.get_half_spec_size(160000), 160), kernel_size=5, strides=4,
-                         activation='relu'))
-        model.add(BatchNormalization())
-        model.add(MaxPool1D(pool_size=(4)))
-        model.add(Conv1D(64, kernel_size=3, activation='relu'))
-        model.add(BatchNormalization())
-        model.add(Flatten())
-        model.add(Dense(256, activation='relu'))
-        model.add(Dense(num_labels, activation='sigmoid'))
 
-        model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['binary_accuracy'])
-        model.summary()
+    all_train_generator = DataGenerator(path + 'train_soundscapes/', all_train_idxs, train_labels, batch_size=50,
+                                    data_length=data_length, num_labels=num_labels)
 
-        model.fit_generator(generator=train_generator, validation_data=val_generator, epochs=4)
-        model.save("/kaggle/working/birds_model.h5")
+    model = Sequential()
+    model.add(Conv1D(64, input_shape=(train_generator.get_half_spec_size(160000), 160), kernel_size=5, strides=4,
+                     activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool1D(pool_size=(4)))
+    model.add(Conv1D(64, kernel_size=3, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(num_labels, activation='sigmoid'))
 
-    model = load_model('birds_model.h5')
+    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['binary_accuracy'])
+    model.summary()
+
+    #model.fit_generator(generator=train_generator, validation_data=val_generator, epochs=4)
+        #model.save("/kaggle/working/birds_model.h5")
+
+    model = load_model('birds_model2.h5')
 
     #results = model.evaluate(val_generator, verbose=1)
     #print('test loss, test acc:', results)
 
-    y_pred = model.predict(test_generator, verbose=1)
+    y_pred = model.predict(all_train_generator, verbose=1)
     y_test = np.where(y_pred > 0.5, 1, 0)
 
     print(y_test.shape)
 
-    for row in samp_subm.index:
+    train_labels['birds_out'] = train_labels['birds']
+
+    for row in train_labels.index:
         string = ''
+        was_nocall = False
         for col in range(len(y_test[row])):
+            if labels[col] == 'nocall':
+                was_nocall = True
+                continue
             if y_test[row][col] == 1:
                 print(col, y_pred[row][col], labels[col])
                 if string == '':
@@ -183,11 +200,120 @@ def main():
                 else:
                     string += ' ' + labels[col]
         if string == '':
-            string = 'nocall'
-        samp_subm.loc[row, 'birds'] = string
+            if was_nocall:
+                string = 'nocall'
+            else:
+                max_col = np.argmax( y_pred[row] )
+                string = labels[max_col]
+        train_labels.loc[row, 'birds'] = string
 
-    output = samp_subm
-    output.to_csv('submission.csv', index=False)
+    output = train_labels
+    output.to_csv('train_submission.csv', index=False)
+
+
+
+
+
+def main2():
+    r_path = "."
+    path = os.path.join(r_path, "./birdclef-2021/")
+
+    train_labels = pd.read_csv(path + 'train_soundscape_labels.csv')
+    train_meta = pd.read_csv(path + 'train_metadata.csv')
+    test_data = pd.read_csv(path + 'test.csv')
+    samp_subm = pd.read_csv(path + 'sample_submission.csv')
+
+    labels = []
+    for row in train_labels.index:
+        labels.extend(train_labels.loc[row, 'birds'].split(' '))
+    labels = list( sorted( set(labels) ) )
+
+    print(labels)
+
+    print('Number of unique bird labels:', len(labels))
+
+    target = pd.Series(index=train_labels.index, name='call')
+
+    for row in train_labels.index:
+        birds = train_labels.loc[row, 'birds'].split(' ')
+        if birds[0] == 'nocall':
+            target[row] = 0
+        else:
+            target[row] = 1
+
+    print(target.value_counts())
+
+    # We set a dummy value for the target label in the test data because we will need for the Data Generator
+    test_data['birds'] = 'nocall'
+
+    target_test = pd.Series(index=train_labels.index, name='call')
+
+    for row in test_data.index:
+        birds = test_data.loc[row, 'birds'].split(' ')
+        if birds[0] == 'nocall':
+            target_test[row] = 0
+        else:
+            target_test[row] = 1
+
+    train_labels = pd.concat([train_labels, target], axis=1)
+    test_data = pd.concat([test_data, target_test], axis=1)
+
+    train_idxs, val_idxs = train_test_split(list(train_labels.index), test_size=0.33, random_state = 100)
+    test_idxs = list(samp_subm.index)
+    all_train_idxs = list( train_labels.index )
+
+    num_labels = len(labels)
+    batch_size = 30
+    data_length = 160000
+
+    print(train_labels.head())
+
+    train_generator = DataGenerator(path + 'train_soundscapes/', train_idxs, train_labels, batch_size=batch_size,
+                                    data_length=data_length, num_labels=num_labels)
+    val_generator = DataGenerator(path + 'train_soundscapes/', val_idxs, train_labels, batch_size=batch_size,
+                                  data_length=data_length, num_labels=num_labels)
+    test_generator = DataGenerator(path + 'test_soundscapes/', test_idxs, test_data, batch_size=1,
+                                   data_length=data_length, num_labels=num_labels)
+
+    all_train_generator = DataGenerator(path + 'train_soundscapes/', all_train_idxs, train_labels, batch_size=50,
+                                    data_length=data_length, num_labels=num_labels)
+
+    model = Sequential()
+    model.add(Conv1D(64, input_shape=(train_generator.get_half_spec_size(160000), 160), kernel_size=5, strides=4,
+                     activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool1D(pool_size=(4)))
+    model.add(Conv1D(64, kernel_size=3, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(2, activation='sigmoid'))
+
+    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['binary_accuracy'])
+    model.summary()
+
+    model.fit_generator(generator=train_generator, validation_data=val_generator, epochs=4)
+
+    model.save("call_model.h5")
+    exit(0)
+
+    model = load_model('call_model.h5')
+
+    #results = model.evaluate(val_generator, verbose=1)
+    #print('test loss, test acc:', results)
+
+    y_pred = model.predict(all_train_generator, verbose=1)
+    y_pred = y_pred[0:train_labels.shape[0], ]
+
+    print(y_pred.shape)
+
+    train_labels['birds_out'] = np.argmax(y_pred, axis = 1)
+
+
+    output = train_labels
+    output.to_csv('train_call.csv', index=False)
+
 
 if __name__ == '__main__':
-    main()
+    #main()
+    main2()
